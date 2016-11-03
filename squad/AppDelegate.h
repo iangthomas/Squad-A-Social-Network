aa3f5df4e0e12eb5f485d21892e46a23b9c9abc5
iangthomas <iangthomas@mac.com>
2016-11-02 22:35:52 -0700

    Initial Commit

---
+++ squad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  squad
+//
+//  Created by Ian Thomas on 11/2/16.
+//  Copyright © 2016 KKIT Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ squad/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  squad
+//
+//  Created by Ian Thomas on 11/2/16.
+//  Copyright © 2016 KKIT Creations. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
